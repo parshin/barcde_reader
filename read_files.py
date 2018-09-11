12c6770819f84f23d9e5bc1d15e44bd7f002f2dd
parshin <ilyaiparshin@gmail.com>
2018-09-11 15:25:09 +0500

    pdf to jpeg

--- read_files.py
+++ read_files.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
-import urllib
 
 import qrtools
 import glob
-import os
 from pyzbar import pyzbar
 import cv2
 import logging
@@ -13,7 +11,11 @@ from conf import nu_addr
 import json
 from base64 import b64encode
 import os
-import urlparse
+from pdf2image import convert_from_path
+
+total_files = 0
+recognized_files = 0
+
 
 def send_barcode(jpg_file, barcode_data):
     logging.info('sending file to service.')
@@ -30,12 +32,23 @@ def send_barcode(jpg_file, barcode_data):
             logging.info('file not_recognized: ' + jpg_file)
 
 
+def pdf_to_jpg():
+    os.chdir("./")
+    pdf_files = glob.glob("*.pdf")
+
+    for pdf_file in pdf_files:
+        convert_from_path(pdf_file, 200, output_folder="./", fmt='jpg')
+
+
 def read_files():
+    global total_files
+    global recognized_files
+
     os.chdir("./")
     orders = glob.glob("*.jpg")
-    logging.info('total files: ' + str(len(orders)))
+    total_files = len(orders)
+    logging.info('total files: ' + str(total_files))
 
-    total_recognized = 0
     qrtools_recognized = 0
     pyzbar_recognized = 0
 
@@ -59,11 +72,11 @@ def read_files():
         if barcode_data == "NULL":
             logging.info("barcode wasn't recognized!")
         else:
-            total_recognized += 1
+            recognized_files += 1
             logging.info("barcode data: " + barcode_data)
             send_barcode(jpgfile, barcode_data)
 
-    logging.info('recognized files: ' + str(total_recognized))
+    logging.info('recognized files: ' + str(recognized_files))
     logging.info("qrtools recognized: " + str(qrtools_recognized))
     logging.info("pyzbar recognized: " + str(pyzbar_recognized))
 
@@ -71,8 +84,12 @@ def read_files():
 if __name__ == "__main__":
     logging.basicConfig(filename='barcodes.log', level=logging.INFO, format='%(levelname)-8s [%(asctime)s] %(message)s')
     logging.info('start reading.')
+    pdf_to_jpg()
     read_files()
-    payload = {'role': 'Ответственный за загрузку заказов КиС', 'user': ''}
-    headers = {'Content-Type': 'application/text;charset=UTF-8'}
-    r = requests.get(nu_addr["nu_address"], params=payload, headers=headers)
+    if total_files > 0:
+        payload = {'role': 'Ответственный за загрузку заказов КиС',
+                   'user': '',
+                   'total_files': total_files,
+                   'recognized_files': recognized_files}
+        r = requests.get(nu_addr["nu_address"], params=payload)
     logging.info('done reading.')
